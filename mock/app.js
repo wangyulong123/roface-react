57f86b3c75b542d24330b10f92dd0e236ffcc6ac
杨松 <syang@amarsoft.com>
2017-12-17 22:11:07 +0800

    1.mock的cors统一处理 2.node-express对restfull的支持处理 3.简化默认处理的主页

--- mock/app.js
+++ mock/app.js
@@ -1,42 +1,52 @@
 const http = require('http');
+var util = require('util');
 const express = require('express');
-const path = require('path');
+
+//CORS统一设置
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials','true');
+    next();
+};
 
 const index = require('./routes/index');
-
 const app = express();
 
-app.use('/', index);
-
+app.use(allowCrossDomain);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    // const err = new Error('Not Found');
+    // err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function (err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+// });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function (err, req, res, next) {
+    const url = req.originalUrl;
+    console.log(`${req.method} ${url}`);
+    next();
 });
 
-const server = http.createServer(app);
+/* ============================================= */
+//这一句一定要放到这里来，否则前端的app.use都不会实现
+/* ============================================= */
 
-server.listen(3003);
-
-function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  console.log(bind);
-}
-
-server.on('error', () => {});
-server.on('listening', onListening);
+app.use('/', index);
+const server = http.createServer(app).listen(3003);
+server.on('listening', function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+})
--- mock/routes/index.js
+++ mock/routes/index.js
@@ -3,11 +3,30 @@ var router = express.Router();
 var path = require('path');
 
 /* GET home page. */
-router.get('/dataform/meta/demoPerson', function(req, res, next) {
-  // /dataform/meta/demoPerson
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.sendFile(path.resolve(__dirname, '../json/demoPerson.json'));
-});
+
+//
+// router.get('/dataform/meta/demoPerson', function (req, res, next) {
+//     // /dataform/meta/demoPerson
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.sendFile(path.resolve(__dirname, '../json/demoPerson.json'));
+// });
+
+router.route("/")
+    .get(function (req, rep, next) {
+        rep.send(" server is running");
+    });
+
+router.route('/dataform/meta/:dataFormId')
+    .get(function (req, rep, next) {
+        // req.params.xxxxx 从path中的变量
+        // req.query.xxxxx 从get中的?xxxx=中
+        // req.body.xxxxx 从post中的变量
+
+        const dataFormId = req.params.dataFormId;
+        rep.setHeader('Content-Type','application/json;charset=utf-8');
+        // rep.setHeader("Access-Control-Allow-Origin", "*");//已经统一放到app.js中统一设置
+        rep.sendFile(path.resolve(__dirname, `../json/${dataFormId}.json`));
+    });
 
 
 module.exports = router;
