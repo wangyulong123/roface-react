da7221543798cb23b1beae20f495b2c1ca1c6ff9
杨松 <syang@amarsoft.com>
2017-12-17 22:29:24 +0800

    info --> detail-info

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Layout } from './layout';
 export { default as Menu } from './menu';
 export { default as Tabs } from './tab';
-export { default as Info } from './info';
+export { default as DetailInfo } from './info';
 export { default as MegaMenu } from './megamenu';
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import 'antd/es/tabs/style';
 
-import { Tabs, Info } from '../components';
+import { Tabs, DetailInfo } from '../components';
 
 import * as rest from '../lib/rest';
 
@@ -23,7 +23,7 @@ export default class Home extends React.Component {
     return (
       <Tabs defaultActiveKey="1" onChange={this.callback}>
         <TabPane tab="Tab 1" key="1">
-          <Info ref="info" dataFormId="demoPerson" />
+          <DetailInfo ref="info" dataFormId="demoPerson" />
         </TabPane>
         <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
         <TabPane tab="Tab 3" key="3">Content of Tab Pane 3</TabPane>
