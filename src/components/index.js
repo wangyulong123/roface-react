ff08f0f6adfc51155ba52144f05b78d50dd7057e
杨松 <syang@amarsoft.com>
2017-12-18 09:50:25 +0800

    feat:1.组件目录，2.rest 参数合并注释案例

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 export { default as Layout } from './layout';
-export { default as Menu } from './menu';
+// export { default as NavTree } from './menu';
+// export { default as NavMega } from './menu';
+// export { default as NavSubtree } from './menu';
 export { default as Tabs } from './tab';
 export { default as DetailInfo } from './info';
 export { default as MegaMenu } from './megamenu';
