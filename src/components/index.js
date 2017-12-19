d4e504629c85d1089995fe09e7bfbbb77df9c6ff
xgwen <xgwen@amarsoft.com>
2017-12-19 09:32:16 +0800

    feat: add ronotify.

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Menu } from './menu';
 export { default as Tabs } from './tab';
 export { default as DetailInfo } from './info';
 export { default as MegaMenu } from './megamenu';
+export { NotificationWithIcon, ModalConfirm } from './ronotify';
