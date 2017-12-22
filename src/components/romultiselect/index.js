43712af01effa6733cd05c0291e2a4104b7e1085
xgwen <xgwen@amarsoft.com>
2017-12-22 11:54:32 +0800

    feat: 修改MultiSelect.

--- src/components/romultiselect/index.js
+++ src/components/romultiselect/index.js
@@ -8,44 +8,69 @@ export default class MultiSelect extends React.Component {
         super(props);
         this.state = {
             value: this.props.value || [],
-            options: this.assembleOptions(this.props.options) || [],
         };
     }
 
-    assembleOptions = (options) => {
-       const a = [];
-       options.forEach((item) => {
-         a.push(<Option key={item}>{item}</Option>);
+    _assembleOptions = (model, options) => {
+        let newOptions;
+        switch(model) {
+            case 'model':
+                newOptions = options.map(item => item.itemName);
+                break;
+            case 'jsonModel':
+                newOptions = options.map(item => item.v);
+                break;
+            case 'additionModel':
+            default:
+                newOptions = options;
+       }
+       const optionArr = [];
+        newOptions.forEach((item) => {
+           optionArr.push(<Option key={item}>{item}</Option>);
        });
-       return a;
+       return optionArr;
     };
 
     handleChange = (value) => {
+        this.setState({
+           value,
+        });
         const { onChange } = this.props;
         if (onChange) {
-            onChange(value);
+            onChange(this.props._changeArray2String(value));
         }
     };
-
+    _changeString2Array = (str) => {
+        if (str instanceof Array) {
+            return str;
+        }
+        if (str === '') {
+            return [];
+        }
+        return str && str.split && str.split(',');
+    };
     _changeArray2String = (ary) => {
+        if (ary.length === 0) {
+            return '';
+        }
         return ary && ary.join && ary.join(',');
     };
-
     render() {
         if (this.props.reading) {
             return (
               <div>{this._changeArray2String(this.props.value)}</div>
             );
         }
+        const children = this._assembleOptions(this.props.model, this.props.options);
         return (
           <Select
             {...this.props}
             mode="multiple"
             style={{width: '100%'}}
-            defaultValue={this.state.value}
+            value={this.state.value}
             onChange={this.handleChange}
           >
-            {this.state.options}
+            {children}
           </Select>
         );
     }
