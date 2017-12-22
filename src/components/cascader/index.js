ee23aa526183be7388c3edadf616bffb713ee977
xgwen <xgwen@amarsoft.com>
2017-12-22 16:49:07 +0800

    feat: create Cascader.

--- src/components/cascader/index.js
+++ src/components/cascader/index.js
@@ -1,47 +1,44 @@
 import React from 'react';
 import { Cascader } from 'antd';
-
-const options = [{
-    value: 'zhejiang',
-    label: 'Zhejiang',
-    children: [{
-        value: 'hangzhou',
-        label: 'Hangzhou',
-        children: [{
-            value: 'xihu',
-            label: 'West Lake',
-        }, {
-            value: 'xiasha',
-            label: 'Xia Sha',
-            disabled: true,
-        }],
-    }],
-}, {
-    value: 'jiangsu',
-    label: 'Jiangsu',
-    children: [{
-        value: 'nanjing',
-        label: 'Nanjing',
-        children: [{
-            value: 'zhonghuamen',
-            label: 'Zhong Hua men',
-        }],
-    }],
-}];
+import { transformIntoTree, cascaderCodemap, attributes } from './tool';
 
 export default class RoCascader extends React.Component{
-    handleChange = (value, selectOptions) => {
-        console.log(value, selectOptions);
+    constructor(props) {
+        super(props);
+        const param = this.props.attributes || attributes;
+        const data = this.props.options || cascaderCodemap;
+        const options = transformIntoTree(data, param);
+        const value = this._changeString2Array(this.props.value);
+        this.state = {
+            value,
+            options,
+        };
+    }
+    handleChange = (value) => {
+        this.setState({
+           value,
+        });
         const { onChange } = this.props;
         if (onChange) {
             onChange(value);
         }
     };
+    _changeString2Array = (str) => {
+        if (str instanceof Array) {
+            return str;
+        }
+        if (str === '') {
+            return [];
+        }
+        return str && str.split && str.split(',');
+    };
     render() {
         return (
           <Cascader
             {...this.props}
-            options={options}
+            value={this.state.value}
+            style={{ width: '100%' }}
+            options={this.state.options}
             onChange={this.handleChange}
           />
         );
