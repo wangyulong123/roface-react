a8dc0b6a66861208e0738edf76a298bdd8506693
liuqiang <qliu1@amarsoft.com>
2017-12-23 15:04:36 +0800

    feat: add info demo

---
+++ app/showcase/detailinfo/index.js
@@ -0,0 +1,6 @@
+export ColumnFour from './columnfour';
+export ColumnThree from './columnthree';
+export ColumnTwo from './columntwo';
+export Group from './group';
+export Simple from './simple';
+export MainFunction from './mainfuction';
