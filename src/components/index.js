efd7f0453ca7601d96d889b3c7903e45a40a863c
杨松 <syang@amarsoft.com>
2017-12-23 11:26:49 +0800

    显示模板列表以及详情模型以及案例数据

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 export { default as Layout } from './layout';
-export { default as NavTree } from './navtree';
-// export { default as NavMega } from './menu';
-// export { default as NavSubtree } from './menu';
-// export { default as FlexTabs } from './menu';
-export { default as FlexTabs } from './tab';
-export { default as DetailInfo } from './info';
 export { default as NavMega } from './megamenu';
+export { default as NavTree } from './navtree';
+// export { default as NavSubtree } from './menu';  //@TODO
+export { default as FlexTabs } from './tab';
+export { default as DetailInfo } from './info';       //@TODO
 export { NotificationWithIcon, ModalConfirm } from './ronotify';
 export { default as Alert } from './alert';
 export { default as Modal } from './modal';
