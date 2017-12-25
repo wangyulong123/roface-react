41cb48fba255fdd300320bc61b2339876db48b7a
rkxie <rkxie@amarsoft.com>
2017-12-25 16:41:57 +0800

    feat: 解决浏览器窗口变化时onresize多次触发问题

--- src/lib/listener.js
+++ src/lib/listener.js
@@ -1,12 +1,24 @@
+const debounce = (fn, delay) => {
+    let timer = null;
+    return () => {
+        let context = this,
+            args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            fn.apply(context, args);
+        }, delay);
+    };
+};
+
 export const addOnResize = (fuc) => {
   let resize;
   if (window.onresize) {
     resize = window.onresize;
   }
-  window.onresize = () => {
-    if (resize) {
-      resize();
-    }
-    fuc();
-  };
+  window.onresize = debounce(() => {
+      if (resize) {
+          resize();
+      }
+      fuc();
+  }, 1000);
 };
