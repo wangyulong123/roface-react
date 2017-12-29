331ece2def709bbe50563261ea876bb1a7021b52
杨松 <syang@amarsoft.com>
2017-12-29 09:47:52 +0800

    参考案例

---
+++ app/ShowCase/LayoutAndComponent/index.js
@@ -0,0 +1,1 @@
+export NoticeCase from './NoticeCase';
--- app/ShowCase/index.js
+++ app/ShowCase/index.js
@@ -1,2 +1,3 @@
 export * as detailInfo from './detailinfo';
 export * as detailTable from './detailtable';
+export * as LayoutAndComponent from './LayoutAndComponent';
