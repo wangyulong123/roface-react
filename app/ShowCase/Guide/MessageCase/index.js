8d16bf5f85f1c5201a1819d1a616f38256882cb8
杨松 <syang@amarsoft.com>
2017-12-30 09:56:41 +0800

    增加参考Starter

---
+++ app/ShowCase/Guide/MessageCase/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class MessageCase extends React.Component {
+
+    render() {
+        return (
+            <Row gutter={10}>
+                <Col span={12} />
+                <Col span={12} />
+            </Row>
+
+
+        );
+    }
+}
+
--- app/ShowCase/Guide/index.js
+++ app/ShowCase/Guide/index.js
@@ -1,3 +1,4 @@
 export HelloWord from './HelloWord';
 export ButtonCase from './ButtonCase';
 export GridLayout from './GridLayout';
+export MessageCase from './MessageCase';
