2a7b5dc182e6f5e8224fb6cca5476abe3d8ad332
liuqiang <qliu1@amarsoft.com>
2017-12-31 12:10:31 +0800

    feat: update path

--- mock/config.js
+++ mock/config.js
@@ -1,7 +1,13 @@
 var redirect = {
   url: 'http://192.168.64.80:8080',
   path: [
-
+    '/dataform/admin/dataform',
+    '/dataform/admin/dataform/:dataFormId/element',
+    '/dataform/admin/dataform/clone',
+    '/dataform/admin/dataform/:id',
+    '/dataform/admin/dataform/:dataFormId/:elementCode',
+    '/dataform/admin/dataform/:id',
+    '/dataform/list/:sort'
   ]
 }
 
