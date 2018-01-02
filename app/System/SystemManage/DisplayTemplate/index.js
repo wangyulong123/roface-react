7cff9c6884aff6564fbda54115e20b911e3912a1
dpcui <dpcui@amarsoft.com>
2018-01-02 21:16:53 +0800

    fix: add ElementDetail dataId edit

--- app/System/SystemManage/DisplayTemplate/index.js
+++ app/System/SystemManage/DisplayTemplate/index.js
@@ -3,60 +3,63 @@ import zhCN from 'antd/lib/locale-provider/zh_CN';
 import './style/index.less';
 import TemplateDetail from './TemplateDetail';
 import ElementDetail from './ElementDetail';
-import { Table, LocaleProvider, Modal, Button, Icon, Notify, Text } from '../../../../src/components';
+import {Table, LocaleProvider, Modal, Button, Icon, Notify, Text} from '../../../../src/components';
 
 export default class DisplayTemplate extends React.Component {
   static TemplateDetail = TemplateDetail;
   static ElementDetail = ElementDetail;
-  constructor(props){
-  super(props);
-  this.id = '';
-  this.state = {
-    data: [],
-    selectedRowRecord: null,
-    pageIndex: 0,
-    pageSize: 10,
-    totalCount: 10,
-    columns: [{
-      title: '#',
-      dataIndex: 'number',
-      key: 'number',
-      render: (text, record, index) => <span>{index}</span>,
-    }, {
-      title: '模板编号',
-      dataIndex: 'code',
-      key: 'code',
-    }, {
-      title: '名称',
-      dataIndex: 'name',
-      key: 'name',
-      render: (text, record) => <a onClick={() => this.createTab(record)}>{text}</a>,
-    }, {
-      title: '分栏数',
-      dataIndex: 'formUIHint',
-      key: 'formUIHint',
-      render: text => <span>{text.columnNumber}</span>,
+
+  constructor(props) {
+    super(props);
+    this.id = '';
+    this.state = {
+      data: [],
+      selectedRowRecord: null,
+      pageIndex: 0,
+      pageSize: 10,
+      totalCount: 10,
+      columns: [{
+        title: '#',
+        dataIndex: 'number',
+        key: 'number',
+        render: (text, record, index) => <span>{index}</span>,
+      }, {
+        title: '模板编号',
+        dataIndex: 'code',
+        key: 'code',
+      }, {
+        title: '名称',
+        dataIndex: 'name',
+        key: 'name',
+        render: (text, record) => <a onClick={() => this.createTab(record)}>{text}</a>,
+      }, {
+        title: '分栏数',
+        dataIndex: 'formUIHint',
+        key: 'formUIHint',
+        render: text => <span>{text.columnNumber}</span>,
       }],
     };
   }
-  componentDidMount(){
-    const { pageIndex, pageSize } = this.state;
+
+  componentDidMount() {
+    const {pageIndex, pageSize} = this.state;
     this._getDataformList(pageIndex, pageSize);
   }
-  _getDataformList = (index, size, code='DESC') => {
-    const { dataform, closeLoading, openLoading } = this.props;
+
+  _getDataformList = (index, size, code = 'DESC') => {
+    const {dataform, closeLoading, openLoading} = this.props;
     openLoading && openLoading();
     dataform.getAdmin(`/dataform/list/code=${code}/${index}-${size}`)
-    .then((res) => {
-      this.setState({
-        pageIndex: res.index,
-        pageSize: res.size,
-        data: res.dataList,
-        totalCount: res.totalRowCount,
-      }, () => {
-        closeLoading && closeLoading();
-      });
-    }).catch((err) => {
+      .then((res) => {
+        this.setState({
+          pageIndex: res.index,
+          pageSize: res.size,
+          data: res.dataList,
+          totalCount: res.totalRowCount,
+        }, () => {
+          closeLoading && closeLoading();
+        });
+      }).catch((err) => {
       Modal.error({
         title: '获取模板列表失败',
         content: JSON.stringify(err),
@@ -75,44 +78,44 @@ export default class DisplayTemplate extends React.Component {
   };
 
   _cloneTableData = () => {
-    const { dataform, openLoading, closeLoading } = this.props;
+    const {dataform, openLoading, closeLoading} = this.props;
     const record = this.state.selectedRowRecord;
     if (!record) {
-    Notify.info({
-    message: '请选择要克隆的模板行！',
-  });
-    return;
-  }
+      Notify.info({
+        message: '请选择要克隆的模板行！',
+      });
+      return;
+    }
     const that = this;
     Modal.confirm({
-    title: '请输入模板编号',
-    content: (
-    <div>
-    <Text defaultValue={record.id} onChange={this._idChange} />
-    </div>
-    ),
-    onOk() {
-    openLoading && openLoading();
-    dataform.postAdmin('/dataform/clone', {
-    newDataFormId: that.id,
-    oldDataFormId: record.id
-  })
-    .then((res) => {
-    that.setState({
-    data: [...that.state.data, res]
-  });
-    closeLoading && closeLoading();
-    Notify.success({
-    message: '克隆成功',
-  });
-  }).catch((e) => {
-    Modal.error({
-    title: '克隆失败',
-    content: JSON.stringify(e),
-  });
-  });
-  },
-  });
+      title: '请输入模板编号',
+      content: (
+        <div>
+          <Text defaultValue={record.id} onChange={this._idChange}/>
+        </div>
+      ),
+      onOk() {
+        openLoading && openLoading();
+        dataform.postAdmin('/dataform/clone', {
+          newDataFormId: that.id,
+          oldDataFormId: record.id
+        })
+          .then((res) => {
+            that.setState({
+              data: [...that.state.data, res]
+            });
+            closeLoading && closeLoading();
+            Notify.success({
+              message: '克隆成功',
+            });
+          }).catch((e) => {
+          Modal.error({
+            title: '克隆失败',
+            content: JSON.stringify(e),
+          });
+        });
+      },
+    });
   };
 
   refresh = () => {
@@ -120,30 +123,30 @@ export default class DisplayTemplate extends React.Component {
   };
 
   createTab = (record) => {
-    const { flexTabs } = this.props;
+    const {flexTabs} = this.props;
     record = record || this.state.selectedRowRecord;
     if (!record) {
-    Notify.info({
-      message: '请选择要查看详情的模板行！',
-    });
+      Notify.info({
+        message: '请选择要查看详情的模板行！',
+      });
       return;
     }
-      const tab = {
+    const tab = {
       id: `System/SystemManage/DisplayTemplate/TemplateDetail/${record.id}`,
       name: `模板:${record.name}`,
       url: 'System/SystemManage/DisplayTemplate/TemplateDetail',
     };
-      flexTabs.createTab({
+    flexTabs.createTab({
       ...tab,
       state: {
-      ...tab,
-      dataId: record.id,
-      flag: record.flag
-    },
+        ...tab,
+        dataId: record.id,
+        flag: record.flag
+      },
     });
-    };
-    _deleteTemplate = () => {
-    const { dataform, closeLoading, openLoading } = this.props;
+  };
+  _deleteTemplate = () => {
+    const {dataform, closeLoading, openLoading} = this.props;
     openLoading && openLoading();
     const record = this.state.selectedRowRecord;
     if (!record) {
@@ -154,27 +157,28 @@ export default class DisplayTemplate extends React.Component {
     }
     // dataform/{id}
     dataform.deleteAdmin(`/dataform/${record.id}`)
-    .then((res) => {
-    closeLoading && closeLoading();
-    Notify.success({
-    message: '删除模板成功',
-  });
-  }).catch((e) => {
-    closeLoading && closeLoading();
-    Modal.error({
-      title: '删除模板失败',
-      content: JSON.stringify(e),
+      .then((res) => {
+        closeLoading && closeLoading();
+        Notify.success({
+          message: '删除模板成功',
+        });
+      }).catch((e) => {
+      closeLoading && closeLoading();
+      Modal.error({
+        title: '删除模板失败',
+        content: JSON.stringify(e),
+      });
     });
-  });
   };
   _paginationOnChange = (page, pageSize) => {
-    this._getDataformList(page-1, pageSize);
+    this._getDataformList(page - 1, pageSize);
   };
   _paginationShowSizeChange = (current, size) => {
-    this._getDataformList(current-1, size);
+    this._getDataformList(current - 1, size);
   };
-  render(){
-    const { pageIndex, pageSize, totalCount, prefix = 'ro' } = this.state;
+
+  render() {
+    const {pageIndex, pageSize, totalCount, prefix = 'ro'} = this.state;
     return (
       <div>
         <div className={`${prefix}-template-detail-button-group`}>
@@ -185,25 +189,25 @@ export default class DisplayTemplate extends React.Component {
               flag: true
             })}
           >
-            <Icon type="plus" />新增模板
+            <Icon type="plus"/>新增模板
           </Button>
           <Button
             onClick={() => this._cloneTableData()}
             className={`${prefix}-template-detail-button-group-button`}
           >
-            <Icon type="close" />克隆
+            <Icon type="close"/>克隆
           </Button>
           <Button
             onClick={() => this.createTab()}
             className={`${prefix}-template-detail-button-group-button`}
           >
-            <Icon type="info" />详情
+            <Icon type="info"/>详情
           </Button>
           <Button
             onClick={() => this._deleteTemplate()}
             className={`${prefix}-template-detail-button-group-button`}
           >
-            <Icon type="info" />删除
+            <Icon type="info"/>删除
           </Button>
         </div>
         <LocaleProvider locale={zhCN}>
