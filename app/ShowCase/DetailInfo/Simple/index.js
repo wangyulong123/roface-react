59a78f9045f8233742ca7226edc4189438e72284
liuqiang <qliu1@amarsoft.com>
2018-01-03 10:59:56 +0800

    feat: compose in component

--- app/ShowCase/DetailInfo/Simple/index.js
+++ app/ShowCase/DetailInfo/Simple/index.js
@@ -3,12 +3,18 @@ import React from 'react';
 import { DetailInfo } from '../../../../src/components';
 
 export default class Simple extends React.Component {
-  _didMount = (info) => {
+  _formReady = (dom) => {
+    console.log(dom)
+  };
+  _dataReady = (info) => {
     info.setItemTemplate('chnName', <div>sdssss</div>);
   };
   render() {
     return (
-      <DetailInfo dataFormId="demo-PersonInfo-data-Simple" didMount={this._didMount} />
+      <DetailInfo
+        dataFormId="demo-PersonInfo-data-Simple"
+        formReady={this._formReady}
+        dataReady={this._dataReady}/>
     );
   }
 }
---
+++ src/components/compose.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const compose = (Com) => {
+  class Compose extends React.Component {
+    shouldComponentUpdate(nextProps) {
+      return this._compareProps(nextProps);
+    }
+    _compareProps = (nextProps) => {
+      const props = ['reading', 'readOnly', 'prefix', 'suffix', 'options', 'value'];
+      return props.some(field => nextProps[field] !== this.props[field]);
+    }
+    render() {
+      return <Com />;
+    }
+  }
+  return Compose;
+};
