e77181f8a71c43e7de30b4766df9dac72c2e078d
liuqiang <qliu1@amarsoft.com>
2018-01-04 19:34:56 +0800

    feat: delete index.js

--- app/Container/App.js
+++ app/Container/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import * as app from '../index';
+import * as app from '../autoIndex';
 
 import { NavMega, FlexTabs } from '../../src/components';
 import PersonalManager from './NavbarComponents/pernalmanager';
--- app/ShowCase/index.js
+++
@@ -1,3 +0,0 @@
-export * as DetailInfo from './DetailInfo';
-export * as DetailTable from './DetailTable';
-export * as Guide from './Guide';
