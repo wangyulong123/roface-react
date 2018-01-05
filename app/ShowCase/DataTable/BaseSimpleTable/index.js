041c1eb2ebd36b42d7970d2c35fdb555ed22a1d3
杨松 <syang@amarsoft.com>
2018-01-05 19:28:37 +0800

    使用规范的数据表格图

--- app/ShowCase/DataTable/BaseSimpleTable/index.js
+++ app/ShowCase/DataTable/BaseSimpleTable/index.js
@@ -3,20 +3,16 @@ import React from "react";
 import { DataTable } from '../../../../src/components';
 
 export default class BaseSimpleTable extends React.Component {
-    didMounted(vm) {
-        vm.run('demo-MapPersonList', {code: 'MenuList'}).then(() => {
-        });
-        vm.setColumnTemplate('name', (row, column, index, text) => {
-            return (<a style={{ backgroundColor: 'green' }}>{text}</a>);
-        });
-
-    }
+    formReady(dom){
+        console.log('form-ready:',dom);
+    };
+    dataReady(data){
+        console.log('data-ready:',data);
+    };
 
     render() {
         return (
-            <div>
-                <DataTable didMounted={this.didMounted}/>
-            </div>
+            <DataTable dataFormId="demo-MapPersonList" dataReady={this.dataReady} tableReady={this.formReady}/>
         );
     }
 }
