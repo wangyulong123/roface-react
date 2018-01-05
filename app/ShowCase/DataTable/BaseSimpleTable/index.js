9d5e5aaa17c432cbf1c958379126c2b21793d126
杨松 <syang@amarsoft.com>
2018-01-05 19:44:45 +0800

    列表可操作列

--- app/ShowCase/DataTable/BaseSimpleTable/index.js
+++ app/ShowCase/DataTable/BaseSimpleTable/index.js
@@ -1,18 +1,30 @@
 import React from "react";
 
-import { DataTable } from '../../../../src/components';
+import { DataTable ,Button} from '../../../../src/components';
 
 export default class BaseSimpleTable extends React.Component {
+    //dataTable,meta,dom
     formReady(dom){
         console.log('form-ready:',dom);
     };
+    //dataTable,dataList
     dataReady(data){
         console.log('data-ready:',data);
     };
 
+    didMounted(vm){
+        vm.setColumnTemplate('name', (row, column, index, text) => {
+            return (<a onClick={()=>this.clickName(row)}>{text}</a>);
+        });
+    };
+
+    clickName(row){
+        console.log(row);
+    };
+
     render() {
         return (
-            <DataTable dataFormId="demo-MapPersonList" dataReady={this.dataReady} tableReady={this.formReady}/>
+            <DataTable dataFormId="demo-MapPersonList" dataReady={this.dataReady} formReady={this.formReady} didMounted={this.didMounted}/>
         );
     }
 }
