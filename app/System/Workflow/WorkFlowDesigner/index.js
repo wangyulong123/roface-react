d5ed33cdfe4a978b2250d9ed7b9bb8e9ebfdf87f
xmzuo <xmzuo@amarsoft.com>
2018-01-06 16:04:53 +0800

    流程模型设计列表

--- app/System/Workflow/WorkFlowDesigner/index.js
+++ app/System/Workflow/WorkFlowDesigner/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import { addOnResize } from '../../../../src/lib/listener';
-import { DataTable ,Button} from '../../../../src/components';
 
-export class WorkflowModelList extends React.Component {
+import {DataTable, Button} from '../../../../src/components';
+
+export default class WorkflowModelList extends React.Component {
     //dataTable(vm),meta,dom
     formReady(dataTable,meta,dom){
         // console.log('form-ready:',dataTable,meta,dom);
     }
     dataReady(dataTable,dataList){
         // console.log('data-ready:',dataTable,dataList);
-        // function clickName(row){
-        //     console.log(row);
-        // }
+        function openWorkflowEditor(row){
+          var url = "http://127.0.0.1:8080/amix/modeler.html?modelId="+row.id;
+          window.open(url);
+        }
 
-        // dataTable.setColumnTemplate('name', (row, column, index, text) => {
-        //     return (<a onClick={()=>clickName(row)}>{text}</a>);
-        // });
+        dataTable.setColumnTemplate('name', (row, column, index, text) => {
+            return (<a onClick={()=>openWorkflowEditor(row)}>{text}</a>);
+        });
     }
+
+    //   System/Workflow/WorkflowDesigner/WorkflowModelList
 
 
     render() {
@@ -24,32 +27,4 @@ export class WorkflowModelList extends React.Component {
             <DataTable dataFormId="workflow-DesignerModelList" dataReady={this.dataReady} formReady={this.formReady}/>
         );
     }
-}
-
-export default class WorkFlowDesigner extends React.Component {
-  static WorkflowModelList = WorkflowModelList;
-
-  constructor(){
-    super();
-    this.height = 150;
-    this.state = {
-      clientHeight: '100%'
-    }
-  }
-  componentDidMount(){
-    this._checkWidth();
-    addOnResize(this._checkWidth);
-  };
-  _checkWidth = () => {
-    this.setState({
-      clientHeight: (document.documentElement.clientHeight - this.height) + 'px'
-    });
-  };
-  render() {
-    return <iframe
-      width="100%"
-      height={this.state.clientHeight}
-      src="http://192.168.64.23:8080/workflow/modeler.html?modelId=252501"
-    >{}</iframe>;
-  }
 }
