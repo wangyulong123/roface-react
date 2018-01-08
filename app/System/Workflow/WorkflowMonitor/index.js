992fc34b8cd88b3deaf5272c9ea61b4867af6019
alenfitz <zhugeluoyu@outlook.com>
2018-01-08 14:20:42 +0800

    feat:update workflowMonitor

--- app/System/Workflow/WorkflowMonitor/index.js
+++ app/System/Workflow/WorkflowMonitor/index.js
@@ -11,9 +11,23 @@ export default class WorkflowModelList extends React.Component {
         // console.log('data-ready:',dataTable,dataList);
         function openWorkflowEditor(row){
             //var url = "http://127.0.0.1:8080/amix/diagram-viewer/index.html?modelId="+row.id;
-            var url = "http://127.0.0.1:8080/amix/diagram-viewer/index.html?processInstanceId="+row.procId+"&processDefinitionId="+row.procDefId;
-            window.open(url);
+            // var url = "http://127.0.0.1:8080/amix/diagram-viewer/index.html?processInstanceId="+row.procId+"&processDefinitionId="+row.procDefId;
+            // window.open(url);
+
+            if (this.flexTabs) {
+                this.flexTabs.createTab({...row,
+                    Com: this._renderComponent(history, row),
+                });
+            }
         }
+        // _menuClick = (item, history) => {
+        //     history.replace(`/${item.id}`);
+        //     if (this.flexTabs) {
+        //         this.flexTabs.createTab({...item,
+        //             Com: this._renderComponent(history, item),
+        //         });
+        //     }
+        // };
 
         dataTable.setColumnTemplate('summary', (row, column, index, text) => {
             return (<a onClick={()=>openWorkflowEditor(row)}>{text}</a>);
@@ -25,7 +39,9 @@ export default class WorkflowModelList extends React.Component {
 
     render() {
         return (
-            <DataTable dataFormId="workflow-ProcInstList" dataReady={this.dataReady} formReady={this.formReady}/>
+            <DataTable dataFormId="workflow-ProcInstList"
+                       dataReady={this.dataReady}
+                       formReady={this.formReady}/>
         );
     }
 }
