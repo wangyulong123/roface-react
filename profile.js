3299f69d10825f6a4aa18ca4f3a8ed1d2ddb614d
jkwu <jkwu@amarsoft.com>
2018-01-10 16:32:32 +0800

    feat: modify flextabs function checkwidth when change

--- profile.js
+++ profile.js
@@ -1,6 +1,6 @@
 const profile = {
-   baseUrl: 'http://localhost:8080/amix',
-   //  baseUrl: 'http://192.168.64.208:8080/amix',
+   // baseUrl: 'http://localhost:8080/amix',
+    baseUrl: 'http://192.168.64.227:8080/amix',
 };
 
 export default profile ;
