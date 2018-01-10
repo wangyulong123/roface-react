952cf3eb6f405d77d23d5ae2f30e56804d72339a
lfzhu <lfzhu>
2018-01-10 17:51:14 +0800

    feat:update dictManage view

--- app/System/Dictionary/DictManager/index.js
+++ app/System/Dictionary/DictManager/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Row, Col, DataTable, DetailInfo, Button} from '../../../../src/components';
+import {Row, Col, DataTable, DetailInfo, Button, Message, Modal} from '../../../../src/components';
 
 export default class DictManager extends React.Component {
 
@@ -8,12 +8,19 @@ export default class DictManager extends React.Component {
         super();
         this.state = {
             dictCode: '_ALL_',
-            infoDisplayType: 'none'
-        }
+            dictItemCode: '0',
+            infoDisplayType: 'none',
+            itemListDisplayType: 'none',
+            itemInfoDisplayType: 'none'
+        };
     }
 
     infoFormReady = (infoApi) => {
         this.infoApi = infoApi;
+    };
+
+    itemInfoFormReady = (infoApi) => {
+        this.itemInfoApi = infoApi;
     };
 
     listDidMounted = (tableApi) => {
@@ -24,46 +31,176 @@ export default class DictManager extends React.Component {
             onclick: this.openDetailInfo,
             name: '新增'
         });
-
+        this.tableApi.addBtn({
+            type: 'danger',
+            onclick: this.deleteOneDictRecord,
+            name: '删除',
+            icon: 'delete',
+        });
 
         this.tableApi.onSelectRow((keys, rows) => {
-            this.setState({dictCode: rows[0].code, infoDisplayType: ''});
-            //this.infoApi.refresh();
+            this.setState({
+                dictCode: rows[0].code,
+                infoDisplayType: '',
+                itemListDisplayType: '',
+                itemInfoDisplayType: 'none'
+            });
+            this.itemListApi.run('system-DictItemList', {dictCode: rows[0].code});
+        });
+    };
+
+    itemListDidMounted = (tableApi) => {
+        this.itemListApi = tableApi;
+
+        this.itemListApi.addBtn({
+            type: 'primary',
+            onclick: this.openItemDetailInfo,
+            name: '新增'
+        });
+        this.itemListApi.addBtn({
+            type: 'danger',
+            onclick: this.deleteOneDictItemRecord,
+            name: '删除',
+            icon: 'delete',
+        });
+
+        this.itemListApi.onSelectRow((keys, rows) => {
+            this.setState({
+                dictCode: rows[0].dictCode,
+                dictItemCode: rows[0].code,
+                infoDisplayType: '',
+                itemInfoDisplayType: ''
+            });
         });
     };
 
     openDetailInfo = () => {
-        this.setState({infoDisplayType: ''});
-    }
+        this.setState({dictCode: '_ALL_', infoDisplayType: '',itemListDisplayType: 'none', itemInfoDisplayType: 'none'});
+    };
+
+    openItemDetailInfo = () => {
+        this.setState({dictCode: this.state.dictCode, dictItemCode: '_ALL_', itemInfoDisplayType: ''});
+    };
+
+    tableRefresh = () => {
+        this.tableApi.run('system-DictList', {dictCode: '_ALL_'});
+    };
+
+    itemtListRefresh = () => {
+        this.itemListApi.run('system-DictItemList', {dictCode: this.state.dictCode});
+    };
+
+    deleteOneDictRecord = () => {
+        const {rest} = this.props;
+        const that = this;
+        if (!that.tableApi.getSelectedRow()) {
+            Message.info('请先选择一行！');
+            return;
+        }
+
+        Modal.confirm({
+            title: '删除确认',
+            content: '您确定删除吗？删除后数据不可恢复！',
+            onOk() {
+                const url = `/dataform/delete/list/system-DictList`;
+                rest.post(url, [{code: that.tableApi.getSelectedRow().code}])
+                    .then(that.tableRefresh);
+            },
+            onCancel() {
+                return;
+            },
+        });
+
+        this.setState({infoDisplayType: 'none', itemListDisplayType: 'none', itemInfoDisplayType: 'none'});
+    };
+
+    deleteOneDictItemRecord = () => {
+        const {rest} = this.props;
+        const that = this;
+        console.log(that.itemListApi.getSelectedRow())
+        return;
+        if (!that.itemListApi.getSelectedRow()) {
+            Message.info('请先选择一行！');
+            return;
+        }
+
+        Modal.confirm({
+            title: '删除确认',
+            content: '您确定删除吗？删除后数据不可恢复！',
+            onOk() {
+                const url = `/dataform/delete/list/system-DictItemList`;
+                rest.post(url, [{
+                    dictCode: that.itemListApi.getSelectedRow().dictCode,
+                    code: that.itemListApi.getSelectedRow().code
+                }])
+                    .then(that.itemtListRefresh);
+            },
+            onCancel() {
+                return;
+            },
+        });
+
+        this.setState({itemInfoDisplayType: 'none'});
+    };
 
     infoSave = () => {
         this.infoApi.saveData((res) => {
-            console.log(res)
+            console.log(res);
+            this.tableRefresh();
         });
-    }
+    };
+
+    itemInfoSave = () => {
+        this.itemInfoApi.saveData((res) => {
+            console.log(res);
+            this.itemtListRefresh();
+        });
+    };
 
     render() {
         return (
             <div>
                 <Row>
                     <Col span={12}>
-                        <DataTable
-                            dataFormId="system-DictList"
-                            dataFormParams={{dictCode: '_ALL_'}}
-                            didMounted={this.listDidMounted}
-                        />
+                        <Row>
+                            <DataTable
+                                dataFormId="system-DictList"
+                                dataFormParams={{dictCode: '_ALL_'}}
+                                didMounted={this.listDidMounted}
+                            />
+                        </Row>
+                        <Row>
+                            <div style={{display: this.state.infoDisplayType}}>
+                                <Button type='primary' onClick={this.infoSave}>保存</Button>
+                                <DetailInfo
+                                    dataFormId="system-DictInfo"
+                                    didMount={this.infoFormReady}
+                                    params={{dictCode: this.state.dictCode}}
+                                />
+                            </div>
+                        </Row>
                     </Col>
 
                     <Col span={12}>
-                        <div style={{display: this.state.infoDisplayType}}>
-                            <DetailInfo
-                                dataFormId="system-DictInfo"
-                                didMount={this.infoFormReady}
-                                params={{dictCode: this.state.dictCode}}
-                            />
-
-                            <Button type='primary' onClick={this.infoSave}>保存</Button>
-                        </div>
+                        <Row>
+                            <div style={{display: this.state.itemListDisplayType}}>
+                                <DataTable
+                                    dataFormId="system-DictItemList"
+                                    dataFormParams={{dictCode: this.state.dictCode}}
+                                    didMounted={this.itemListDidMounted}
+                                />
+                            </div>
+                        </Row>
+                        <Row>
+                            <div style={{display: this.state.itemInfoDisplayType}}>
+                                <Button type='primary' onClick={this.itemInfoSave}>保存</Button>
+                                <DetailInfo
+                                    dataFormId="system-DictItemInfo"
+                                    didMount={this.itemInfoFormReady}
+                                    params={{dictCode: this.state.dictCode, dictItemCode: this.state.dictItemCode}}
+                                />
+                            </div>
+                        </Row>
                     </Col>
                 </Row>
             </div>
