3e33194f3311b691dccedc606afb03e6fef4d68a
wkchen <www.wkchen@amarsoft.com>
2018-01-10 10:07:05 +0800

    feat:更改URL拼接方式

--- app/System/Workflow/WorkflowDesigner/index.js
+++ app/System/Workflow/WorkflowDesigner/index.js
@@ -9,8 +9,8 @@ export default class WorkflowModelList extends React.Component {
     formReady = (dataTable,meta,dom) => {
         // console.log('form-ready:',dataTable,meta,dom);
     }
-    // dataReady(dataTable,dataList){
-    //     // console.log('data-ready:',dataTable,dataList);
+
+    // console.log('data-ready:',dataTable,dataList);
     //     function openWorkflowEditor(row){
     //       var url = "http://127.0.0.1:8080/amix/modeler.html?modelId="+row.id;
     //       window.open(url);
@@ -19,7 +19,57 @@ export default class WorkflowModelList extends React.Component {
     //     dataTable.setColumnTemplate('name', (row, column, index, text) => {
     //         return (<a onClick={()=>openWorkflowEditor(row)}>{text}</a>);
     //     });
-    // }
+
+    dataReady = (dataTable,dataList,vm) =>{
+        const resultModal = Modal;
+        const deploy = (row) => {
+            rest.put("/workflow/model/"+row.id+"/deploy")
+                .then((res) => {
+                    resultModal.info({
+                        title: '返回信息提示',
+                        content: res.result
+                    })
+                });
+        }
+
+
+        const refresh = () => {
+            const { refresh } = this.props;
+            refresh && refresh();
+        }
+
+        const openWorkflowEditor = (row) => {
+            // var url = "http://127.0.0.1:8080/amix/modeler.html?modelId="+row.id;
+            var url = rest.getRequestURL(`/modeler.html?modelId=${row.id}`);
+            window.open(url);
+        }
+
+        const addModel = () => {
+            rest.put("/workflow/model/addModel").then(()=>{
+                refresh()
+            })
+        }
+
+
+        vm.setColumnTemplate('button', (row) => {
+            return (<div><Button onClick={()=>openWorkflowEditor(row)}>流程设计</Button>
+                <Dropdown overlay={
+                    <Menu>
+                        <Menu.Item onClick={()=>deploy(row)}>流程部署</Menu.Item>
+                        <Menu.Item>流程导入</Menu.Item>
+                    </Menu>
+                }>
+                    <Button>操作</Button>
+                </Dropdown>
+            </div>);
+        });
+
+        vm.addBtn({
+            type: 'primary',
+            onclick: addModel,
+            name: '新建模型',
+        });
+    }
 
     didMounted = (vm) => {
         const resultModal = Modal;
@@ -40,7 +90,8 @@ export default class WorkflowModelList extends React.Component {
         }
 
         const openWorkflowEditor = (row) => {
-            var url = "http://127.0.0.1:8080/amix/modeler.html?modelId="+row.id;
+            // var url = "http://127.0.0.1:8080/amix/modeler.html?modelId="+row.id;
+            let url = rest.getRequestURL(`/modeler.html?modelId=${row.id}`);
             window.open(url);
         }
 
@@ -78,7 +129,8 @@ export default class WorkflowModelList extends React.Component {
             <DataTable dataFormId="workflow-DesignerModelList"
                        // dataReady={this.dataReady}
                        formReady={this.formReady}
-                       didMounted={this.didMounted}/>
+                       didMounted={this.didMounted}
+            />
         );
     }
 }
