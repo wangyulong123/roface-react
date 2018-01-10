47fa6f821ebec026386b5ce414696e84b243d96e
ylu2 <ylu2@amarsoft.com>
2018-01-10 16:33:36 +0800

    feat:modify role manager page

--- app/System/Organization/RoleManager/index.js
+++ app/System/Organization/RoleManager/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Row, Col, DataTable, Tabs, DetailInfo} from '../../../../src/components';
+import {Row, Col, DataTable, Tabs, DetailInfo, Button} from '../../../../src/components';
 const TabPane = Tabs.TabPane;
 
 export default class OrgManager extends React.Component {
@@ -8,65 +8,89 @@ export default class OrgManager extends React.Component {
         this.state = {
             id: 1,
         };
-        this.infoApi = null;
+        this.info = null;
         this.key = null;
-        this.vm = null;
+        this.tableData = null;
     }
 
-    _listFormReady = (dataTable, meta, dom) => {
+    listFormReady = (dataTable, meta, dom) => {
         console.warn(dataTable, meta, dom);
     };
 
-    _infoFormReady = (infoApi) => {
-        this.infoApi = infoApi;
+    roleUserListFormReady = (dataTable, meta, dom) => {
+        console.warn(dataTable, meta, dom);
+    };
+    roleUserListDidMounted = (api) => {
+
+    }
+
+
+
+    infoDataReady = (infoApi) => {
+        this.info = infoApi;
     };
 
-    _listDidMounted = (api) => {
-        const vm = api;
-        const that = this;
-        this.vm = vm;
+    saveData = () => {
+        this.info.saveData((error,values) => {
+            if (error == null) {
+                this.info.setData(values);
+                this.tableData.run("system-RoleList",{code: 'RoleList'});
+            }
+        });
+    };
 
-        vm.addBtn({
+
+
+    listDidMounted = (api) => {
+        const dataTable = api;
+        const that = this;
+        this.tableData = dataTable;
+
+        const appendRow = () => {
+            this.info.refresh();
+        };
+
+        const removeRows = () => {
+            const rows = dataTable.getSelectedRows();
+            if (!rows || !rows.length) {
+                message.warning('请选择要删除的行数据');
+                return;
+            }
+            dataTable.removeRows(rows);
+        }
+
+        dataTable.addBtn({
             type: 'primary',
             onclick: appendRow,
             name: '新增',
         });
+        dataTable.addBtn({
+            type: 'primary',
+            onclick: removeRows,
+            name: '删除',
+        });
 
-        vm.setSelectedRows((row, index) => {
+        dataTable.setSelectedRows((row, index) => {
             console.log(index);
             return index === 0;
         });
 
-        function appendRow() {
-            const row = {
-                name: 'SomeoneRear',
-                type: 'some'
-            };
-            vm.appendRow(row);
-        };
 
-        vm.onSelectRow((selectedKeys, selectedRows) => {
-            const rows = vm.getData();
+
+        dataTable.onSelectRow((selectedKeys, selectedRows) => {
+            const {closeLoading, openLoading} = this.props;
+            openLoading && openLoading();
+            const rows = dataTable.getData();
             const rowsIdx = rows.filter((row) => {
                 return row.$$key === selectedRows[0].$$key;
             })[0];
             that.key = rowsIdx.$$key;
-            that.infoApi.setData({
-                code: rowsIdx && rowsIdx.code,
-                name: rowsIdx && rowsIdx.name,
-                type: rowsIdx && rowsIdx.type,
-                status: rowsIdx && rowsIdx.status
-            });
+            console.log(that);
+            //this.setState({id: rowsIdx.id});
+            that.info.refresh({params: {id: rowsIdx.id}},() => {closeLoading && closeLoading()});
+
         });
     };
-
-    _infoValuesChange = (values) => {
-        const rows = vm.getData();
-        const rowsIdx = rows.filter((row) => {
-            return row.$$key === this.key;
-        })[0];
-
-    }
 
 
 
@@ -78,22 +102,28 @@ export default class OrgManager extends React.Component {
                 <Row>
                     <Col span={12}>
                         <DataTable
-                            dataFormId="system-AdminRoleList"
-                            dataFormParams={{code: 'AdminRoleList'}}
-                            formReady={this._listFormReady}
-                            didMounted={this._listDidMounted}
+                            dataFormId="system-RoleList"
+                            dataFormParams={{code: 'RoleList'}}
+                            formReady={this.listFormReady}
+                            didMounted={this.listDidMounted}
                         />
                     </Col>
                     <Col span={12}>
                         <Tabs defaultActiveKey="1">
                             <TabPane tab="基本信息" key="1">
-                               <DetailInfo dataFormId="system-AdminRoleInfo"
-                                           didMount={this._infoFormReady}
-                                           onValuesChange={this._infoValuesChange}
+                               <DetailInfo dataFormId="system-RoleInfo"
+                                           dataReady={this.infoDataReady}
                                 />
+                                <Button type="primary" onClick={() => this.saveData()} >保存</Button>
                             </TabPane>
                             <TabPane tab="权限" key="2">Content of Tab Pane 2</TabPane>
-                            <TabPane tab="角色下用户" key="3">Content of Tab Pane 3</TabPane>
+                            <TabPane tab="角色下用户" key="3">
+                                <DataTable dataFormId="system-UserListForRoleManage"
+                                           dataFormParams={{roleId: 1}}
+                                           formReady={this.roleUserListFormReady}
+                                           didMounted={this.roleUserListDidMounted}
+                                />
+                            </TabPane>
                         </Tabs>
                     </Col>
                 </Row>
